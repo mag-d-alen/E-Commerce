75611b33705b4472932101770934d4c7fd2b22e2
Magdalena BenBassatLuszczynska <magdalena@a-i.com>
2022-11-28 15:37:40 +0200

    removing unised files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,6 @@ import type { AppProps } from "next/app";
 import { StateContext } from "../state/StateContext";
 import { Toaster } from "react-hot-toast";
 import { Navbar } from "../components/Navbar";
-import { Head } from "next/document";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
